1fe6bb3933f5fb882afb1801a984ad9578c26819
Unai <unai.v@hastee.com>
2026-04-21 08:05:28 +0200

    Add global styles and design system CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { StatsPage } from './pages/StatsPage'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
